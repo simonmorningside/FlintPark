3a9a346bc0cac2b142bf30017a7188a7d9b8f214
Mackailee Longobricco <mrl010@morningside.edu>
2024-11-19 10:41:53 -0600

    update2

--- src/routes/streets.jsx
+++ src/routes/streets.jsx
@@ -1,34 +1,33 @@
 // Import React and useState hook
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // Define a functional component called ExamplePage
 export default function Streets(){
-  // Define a piece of state called 'showInfo' and initialize it to 'false'
-  const [showInfo, setShowInfo] = useState(false);
-
-  // Toggle function to change the 'showInfo' state between true and false
-  const toggleInfo = () => {
-    setShowInfo(!showInfo);
-  };
-
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Welcome to My Example Page!</h1>
-      <p>This is a simple page built with React.</p>
-
-      {/* Button to toggle additional info */}
-      <button onClick={toggleInfo}>
-        {showInfo ? 'Hide Info' : 'Show Info'}
-      </button>
-
-      {/* Conditionally render additional information based on 'showInfo' state */}
-      {showInfo && (
-        <div style={{ marginTop: '20px' }}>
-          <p>This will link to <Link to={`/stjohns/`}>StJohn</Link></p>
-          <p>This will link to <Link to={`/flintfloralpark/`}>FlintFloralPark</Link></p>
+      return (
+        <div className="streets-container">
+          {/* Left Section: Description & Links */}
+          <div className="streets-description">
+            <h2>Explore Flint Floral Park Streets</h2>
+            <p>
+              Use the interactive map on the right to easily navigate through the streets
+              of the Flint Floral neighborhood. Simply click on a street on the map for more
+              information. Alternatively, you can use the links below to directly visit specific
+              streets and find out more about them.
+            </p>
+    
+            <ul className="streets-links">
+            <p>Search the streets of <Link to={`/stjohns/`}>StJohn</Link></p>
+            <p>Search the streets of <Link to={`/flintfloralpark/`}>FlintFloralPark</Link></p>
+            </ul>
+          </div>
+    
+          {/* Right Section: Interactive Map */}
+          <iframe
+            src="https://storage.googleapis.com/flint-floral-park/map/TestQGIS/index.html"
+            className="streets-map"
+            title="Interactive Map"
+          ></iframe>
         </div>
-      )}
-    </div>
-  );
-};
+      );
+    }
