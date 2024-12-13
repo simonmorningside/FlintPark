adaeea625d7b03204477589016701ad770fe6e32
Simon Zychowski <saz002@morningside.edu>
2024-12-13 13:06:58 -0600

    i may have broke it on accident

--- src/routes/stjohns.jsx
+++ src/routes/stjohns.jsx
@@ -14,20 +14,4 @@ export default function StJohn () {
             </Form>
         </div>
     )
-}import { Form } from "react-router-dom";
-
-export default function StJohn () {
-    return (
-        <div>
-            <h1>St Johns</h1>
-            <p>Flint Foral Neighborhood</p>
-            <Form>
-                <label>
-                    Name:
-                    <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
-            </Form>
-        </div>
-    )
 }
