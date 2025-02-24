b935439f942679c99d70889da2e201031fea8ba3
Simon Zychowski <saz002@morningside.edu>
2025-02-24 14:15:24 -0600

    Big changes

--- src/routes/mtOliveArchive/pastors.jsx
+++ src/routes/mtOliveArchive/pastors.jsx
@@ -77,20 +77,25 @@ export default function Pastors() {
 
 <section className="founders-section">
         <div className="founders-header">
-          <h2 className="founders-header-text">Pastors</h2>
+          <h2 className="founders-header-text">Rev. Wade Hampton McKinney</h2>
         </div>
         <div className="founders-images-div" style={{ display: 'flex', alignItems: 'center' }}>
-          <button onClick={handlePrev} disabled={startIndex === 0} style={{ marginRight: '10px' }}>
-            <ChevronLeft size={24} />
-          </button>
-          <div className="founders-images" style={{ display: 'flex', gap: '10px' }}>
-            {pastors.images.slice(startIndex, startIndex + imagesPerPage).map((image, index) => (
-              <img key={index} src={image.url} alt="Pastor" className="pastor-photo" />
-            ))}
+        <img src={pastors.images[0]} alt="Rev. Wade Hampton McKinney" className="founders-image" />
           </div>
-          <button onClick={handleNext} disabled={startIndex + imagesPerPage >= pastors.images.length} style={{ marginLeft: '10px' }}>
-            <ChevronRight size={24} />
-          </button>
+          <div>
+            <p>
+            Wade McKinney (1892-1963) was born in Cleveland, Ga., to Wade and Mary Brown McKinney. He attended Atlanta Baptist College Academy, Morehouse College, and Colgate Rochester Theological Seminary and served in the U.S. Army in World War I. He came to Mt Olive in 1923 and lived at 1617 Harrison St. until moving to the parsonage at 1608 Park St. In 1924 he married Annie Ruth Berry (1900-1966), from Birmingham, Alabama. Ruth Berry McKinney had been educated at Spelman College and Columbia University. 
+
+By the time he began in 1923, the mass migration to Flint had begun and the church was ramping up to meet the needs of recent migrants in the city. In June 1924 a singing group known as “The Ford Eight” visited Flint and performed at Mt Olive. The entrance fee was 25 cents. In 1925 the NAACP met at Mt Olive. In November of that same year a week-long series of evangelical events was held at Mt Olive with a visiting preacher from Connecticut. Miss Dorothy Green sang “ Steal Away to Jesus” and Mrs. Ruth McKinney sang “Stand By Me.”
+
+During McKinney’s ministry, the church grew from less than 70 members to over 300. He established a church administrative structure, missionary society, a chapter of the Baptist Young People’s Union, the Ada Berry Bible class (named after his mother in law), Women's Day, a Senior Usher Board, a choir, a deacon board, and the trustee board. Rev. McKinney attended the Michigan-Ontario Baptist convention in Windsor in 1925 and read the convention reports for his evening sermon. In 1925 the membership was estimated at 200 (with 75 owning or purchasing their homes) when Rev. R.L. Bradby from Detroit’s Second Baptist and president of the Michigan-Ontario Baptist Convention launched a building fund for Mt Olive. The American Baptist Home Mission Society engaged an architect, but no building occurred until 1956. In 1926 services featured both a senior and a junior choir and performed songs such as Harry Burleigh’s “Deep River,” Felix Mendelsson’s “Oh Rest in the Lord,” and Holton's “The Chimes of the Holy Night.”
+
+The McKinney family moved to Cleveland in 1928 to lead the large but divided Antioch Church where they became religious and community leaders who fought for civil rights. Rev. Wade Hampton McKinney is featured in the Morehouse College International Hall of Honor and his family papers and photos are on file at Cleveland Public LIbrary. 
+
+
+(photo: Morehouse College International Hall of Honor portrait)
+
+            </p>
           </div>
         </section>
       </>
