a7105f070b0723fa4aaadc514319493efce2ec39
Simon Zychowski <saz002@morningside.edu>
2025-03-13 16:02:13 -0500

    Finally figured out the pdfs yippeeee

--- src/routes/mtOliveArchive/lifeEvents.jsx
+++ src/routes/mtOliveArchive/lifeEvents.jsx
@@ -9,20 +9,28 @@ import '../../mtolivearchive.css';
 
 export default function LifeEvents() {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({ urls: []});
+  const [pdfs, setpdfs] = useState({ jpeg: [], life: [] });
 
 
   useEffect(() => {
     // Replace with your actual API URL
-    fetch("https://floral-park-webserver-861401374674.us-central1.run.app/api/pdf")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log("Received Data:", json); // Logs data to the console
-        setData(json); // Store the data in state if needed
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error); // Logs any errors
-      });
+    const fetchPdfs = async () => {
+      try {
+        const response = await fetch('https://floral-park-webserver-861401374674.us-central1.run.app/api/pdf');
+        if (!response.ok) {
+          throw new Error('Failed to fetch media data');
+        }
+        const data = await response.json();
+        console.log('Fetched Media Data:', data);
+        setpdfs(data);
+      } catch (error) {
+        console.error('Error fetching media:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPdfs();
   }, []);
 
 
@@ -37,8 +45,8 @@ export default function LifeEvents() {
 <section className="founders-section">
           <h2 className="founders-header-text">Rev. Wade Hampton McKinney</h2>
           <div>
-            <iframe src={data.urls[2]} width="600px" height="800px" title="Life Events"></iframe>
-            <image src={data.urls[8]} alt="Rev. Wade Hampton McKinney" />
+            <iframe src={pdfs.life[0].url} title="Rev. Wade Hampton McKinney" className = "pdf-iframe"></iframe>
+            <img src={pdfs.jpeg[1].url} alt="Rev. Wade Hampton McKinney" className = "pdf-image"></img>
           </div>
         </section>
       </>
