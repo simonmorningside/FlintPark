f0b71488dc2a676229c37e256e9709a01755568b
Simon Zychowski <saz002@morningside.edu>
2025-03-28 12:20:41 -0500

    mayhbe this will fix it?

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'PLUGIN_ERROR' && warning.plugin === '@vitejs/plugin-react-swc') {
-          console.error('Failed to load @vitejs/plugin-react-swc:', warning.message);
-        } else {
-          warn(warning);
-        }
-      }
-    }
-  }
-});
+})
