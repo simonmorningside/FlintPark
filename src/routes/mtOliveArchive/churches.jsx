59aa9f047f1e7c310293ffe930ca459995367b9c
Mackailee Longobricco <mrl010@morningside.edu>
2025-05-07 09:43:02 -0500

    image update

--- src/routes/mtOliveArchive/churches.jsx
+++ src/routes/mtOliveArchive/churches.jsx
@@ -12,7 +12,7 @@ export default function Churches() {
   const [pastors, setPastors] = useState({ images: [], videos: [] });
   const [startIndex, setStartIndex] = useState(0);
   const [imagesPerPage, setImagesPerPage] = useState(3); // default
-  const selectedImageIndices = [5, 35, 9, 43, 16, 13, 40, 50, 51, 52, 49, 46, 7, 53, 19, 10, 45, 1, 48, 47];
+  const selectedImageIndices = [5, 54, 9, 43, 16, 13, 40, 50, 51, 52, 49, 46, 7, 53, 19, 10, 45, 1, 48, 47];
   const [selectedFounderIndex, setSelectedFounderIndex] = useState(null);
   const [selectedBuildingIndex, setSelectedBuildingIndex] = useState(null);
   const [selectedParsonageIndex, setSelectedParsonageIndex] = useState(null);
@@ -168,7 +168,7 @@ export default function Churches() {
               <div className="founders-info-container">
                 <div className="founders-content-row">
                   <div className="founders-images">
-                    {[18, 0, 0].map((idx, i) => (
+                    {[18, 0, 19].map((idx, i) => (
                       media.images[idx] && (
                         <div className="founder-box" key={i}>
                           <img
