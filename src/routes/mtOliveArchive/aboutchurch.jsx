fc8edc37a88bb48328c07ec571d48878702523bb
Simon Zychowski <saz002@morningside.edu>
2025-11-13 11:30:40 -0600

    final website edits

--- src/routes/mtOliveArchive/aboutchurch.jsx
+++ src/routes/mtOliveArchive/aboutchurch.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import TimeLine from './timeline';
+import melva from '../../images/melva.png';
 import '../../styles/index.css';
 import '../../fillerstylepageuntilwearesorted.css';
 import '../mtOliveArchive/mtOliveArchive.css';
@@ -11,45 +12,49 @@ export default function AboutChurch() {
 
   return (
     <div className="center-page-about">
+      <h1 className="color">The Mt. Olive Missionary Baptist Church History Project</h1>
+      <img src={melva} alt="Mt. Olive Missionary Baptist Church" className="about-church-image" />
       <div className="left-justify-about">
-        <h2>The Archive Project Team</h2>
-          <p>The Mt. Olive Missionary Baptist History project is a collaboration between the Mt. Olive History Ministry and the Flint Neighborhood History Project.</p>
-          <p>The items in the Mt. Olive Missionary Baptist Church archive were gathered by members of the History Ministry between 2023 and 2025 under the leadership of Melva Viltz. Members of the archive working group include Betty Tippor, Wanda Harden, Alfreda Duncan, and Earnest and Nancy Pouncy. Items were also shared by Kenneth Gillard. The collection was preserved, catalogued, and digitized by Natalie Frary under the direction of Vickie Larsen and Pamela Hawkins of the Flint Neighborhood History Project. The digitization protocols and metadata structure were developed by Natalie Frary and Vickie Larsen in consultation with Anne Cong-Huyen, Joe Bauers, and Matt Caruthers, members of the Anti-Racist Digital Research Initiatives support team at the University of Michigan College of Literature Science and the Arts. Archive-quality digital files are owned by Mt. Olive Missionary Baptist church.</p>
+        <p>This project honors the memory of Melva Viltz (1951-2025) and her two decades of dedication to the Mt. Olive Historical Ministry. The video history documentary, the church materials in this archive, and the successful completion of this project testify to her leadership.</p>
+        <p>The Mt. Olive Missionary Baptist Church History project is a collaboration between the Mt. Olive Historical Ministry and the Flint Neighborhood History Project.</p>
       </div>
       <div className="left-justify-about">
+        <h2>The Archive Project Team</h2>
+        <p>The items in the Mt. Olive Missionary Baptist Church archive were gathered by members of the Historical Ministry between 2023 and 2025 under the leadership of Melva Viltz. Members of the archive working group included Betty Tippor, Wanda Harden, Alfreda Duncan, and Earnest and Nancy Pouncy. Items were also shared by Rev. Kenneth Gillard. The collection was preserved, catalogued, and digitized by Natalie Frary under the direction of Vickie Larsen and Pamela Hawkins of the Flint Neighborhood History Project. The digitization protocols and metadata structure were developed by Natalie Frary and Vickie Larsen in consultation with Anne Cong-Huyen, Joe Bauers, and Matt Caruthers, members of the Anti-Racist Digital Research Initiatives support team at the University of Michigan College of Literature Science and the Arts. Archive-quality digital files of the entire collection are held and owned by Mt. Olive Missionary Baptist Church.</p>      </div>
+      <div className="left-justify-about">
         <h2>The Research</h2>
-          <p>The research for the written history in these pages was conducted by Vickie Larsen of Morningside University and Natalie Frary of the University of Michigan-Flint. It is based upon historical narratives in the church archive and on primary sources including United States and Canadian census data; birth, marriage, military, and death records; City Directories; cemetery records; the Digital Archives of Ontario; Genesee County property records; the 1915 Sanborn insurance maps of Flint; and newspapers including The Flint Journal, The Lansing Journal, The Michigan Democrat, The Freeman, and the Bronze Reporter. Secondary sources used in developing this history are as follows:</p>
-            <ul>
-              <li>Amherstburg Regular Missionary Baptist Association, A History of the Amherstburg Regular Missionary Baptist Association, Its Auxiliaries and Churches: Compiled from the Minutes and Historical Essays, 1940.</li>
-              <li>Crowder, Norman Kenneth. Early Ontario Settlers: A Source Book. Genealogical Pub. Co., Baltimore, 1993.</li>
-              <li>De B’béri, Boulou Ebanda, Nina Reid-Maroney, Handel Kashope Wright, and Afua Cooper. The Promised Land: History and Historiography of the Black Experience in Chatham-Kent's Settlements and Beyond. Series: African & Diasporic Cultural Studies. University of Toronto Press, 2014.</li>
-              <li>Kingsville-Gosfield Heritage Society, Kingsville 1790-2000: A Stroll Through Time, 2 vols. Kingsville-Gosfield Heritage Society, 2003.</li>
-              <li>Smardz Frost, Karolyn, Veta Smith Tucker, and David W. Blight, editors. A Fluid Frontier : Slavery, Resistance, and the Underground Railroad in the Detroit River Borderland. Wayne State University Press. Detroit, 2016.</li>
-            </ul>
+        <p>The research for the written history in these pages was conducted by Vickie Larsen of Morningside University and Natalie Frary of the University of Michigan-Flint. It is based upon historical narratives in the church archive and on primary sources including United States and Canadian census data; birth, marriage, military, and death records; City Directories; cemetery records; the Digital Archives of Ontario; Genesee County property records; the 1915 Sanborn insurance maps of Flint; and newspapers including The Flint Journal, The Lansing Journal, The Michigan Democrat, The Freeman, and the Bronze Reporter. Secondary sources used in developing this history include:</p>
+        <ul>
+          <li>Amherstburg Regular Missionary Baptist Association, A History of the Amherstburg Regular<br/>Missionary Baptist Association, Its Auxiliaries and Churches: Compiled from the Minutes and Historical Essays, 1940.</li>
+          <li>Crowder, Norman Kenneth. Early Ontario Settlers: A Source Book. Genealogical Pub. Co., Baltimore, 1993.</li>
+          <li>De B’béri, Boulou Ebanda, Nina Reid-Maroney, Handel Kashope Wright, and Afua Cooper. The Promised Land: History and Historiography of the Black Experience in Chatham-Kent's Settlements and Beyond. Series: African & Diasporic Cultural Studies. University of Toronto Press, 2014.</li>
+          <li>Kingsville-Gosfield Heritage Society, Kingsville 1790-2000: A Stroll Through Time, 2 vols. Kingsville-Gosfield Heritage Society, 2003.</li>
+          <li>Smardz Frost, Karolyn, Veta Smith Tucker, and David W. Blight, editors. A Fluid Frontier : Slavery, Resistance, and the Underground Railroad in the Detroit River Borderland. Wayne State University Press. Detroit, 2016.</li>
+        </ul>
       </div>
       <div className="left-justify-about">
         <h2>Digital Development</h2>
-          <p>Digital video and audio editing was performed by Vickie Larsen. Digitized materials were converted into accessible formats to ensure compatibility with web viewing and were configured for a searchable database for web viewing by student developers Simon Zychowski and Mackailee Longobricco of Morningside University. These same developers developed the user interface.</p>
+        <p>Digital video and audio editing was performed by Vickie Larsen. Digitized materials were converted into accessible formats to ensure compatibility with web viewing and were configured for a searchable database by student developers Simon Zychowski and Mackailee Longobricco of Morningside University. This website, its backend, and data are hosted on the google cloud platform. A github repository sends updates to the cloud deployed site.</p>
       </div>
       <div className="left-justify-about">
         <h2>Digital Deployment</h2>
-          <p>This website is hosted on the google cloud platoform with the backend and data also functioning from there. It has a repository on github that sends automatic updates to the cloud deployed site whenever the main branch is edited. This allows modification for future work from other humanities passionate CS majors. Allowing the archive to grow and be updated as our knowlege about the Mt. Olive history grows.</p>
+        <p>This website is hosted on the google cloud platoform with the backend and data also functioning from there. It has a repository on github that sends automatic updates to the cloud deployed site whenever the main branch is edited. This allows modification for future work from other humanities passionate CS majors. Allowing the archive to grow and be updated as our knowlege about the Mt. Olive history grows.</p>
       </div>
       <div className="left-justify-about">
-        <h2>Additions and Concerns</h2>
-          <p>Additions to the physical or digital Mt. Olive Missionary Baptist Church archive can be shared with members of the History Ministry or brought to the church office. Please direct any revisions or corrections to existing materials or text to Vickie Larsen at larsenv@morningside.edu</p>
+        <h2>Additions and Corrections</h2>
+        <p>Additions to the physical or digital Mt. Olive Missionary Baptist Church archives can be shared with members of the Historical Ministry, currently led by Betty Tipper. Please direct any revisions or corrections to existing materials or text to Betty Tipper or to Vickie Larsen at larsenv@morningside.edu</p>
       </div>
       <div className="left-justify-about">
         <h2>Permissions</h2>
-          <p>The items on this site are covered by a Non-Commercial Creative Commons License. Members of the public are encouraged to use its contents for non-commercial purposes. Individuals interested in using materials for commercial purposes must contact a member of the Mt. Olive Missionary Baptist History Ministry for permission to do so.</p>
+        <p>The items on this site are covered by a Non-Commercial Creative Commons License. Members of the public are encouraged to use its contents for non-commercial purposes. Individuals interested in using materials for commercial purposes must contact a member of the Mt. Olive Missionary Baptist History Ministry for permission to do so.</p>
       </div>
       <div className="left-justify-about">
         <h2>Media Credits</h2>
-          <p>
-            This Far By Faith, Mt. Olive Missionary Baptist Church, Flint
-            Choir Recordings, Mt. Olive Missionary Baptist Church, Flint and
-            Zion Baptist Church, Washington DC
-          </p>
+        <p>
+          This Far By Faith, Mt. Olive Missionary Baptist Church, Flint
+          Choir Recordings, Mt. Olive Missionary Baptist Church, Flint and
+          Zion Baptist Church, Washington DC
+        </p>
       </div>
     </div>
   );
